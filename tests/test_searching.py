ffd2b30001222a884b8143d168a120dd828af1a7
Mohammed Saqib <113831139+saqib777@users.noreply.github.com>
2025-09-08 09:22:56 +0530

    Update test_searching.py

--- tests/test_searching.py
+++ tests/test_searching.py
@@ -6,5 +6,10 @@ class TestSearching(unittest.TestCase):
         self.assertEqual(binary_search([1, 2, 3, 4, 5], 3), 2)
         self.assertEqual(binary_search([1, 2, 3, 4, 5], 6), -1)
 
+    def test_linear_search(self):   # 👈 New test cases
+        self.assertEqual(linear_search([1, 2, 3, 4, 5], 3), 2)
+        self.assertEqual(linear_search([1, 2, 3, 4, 5], 6), -1)
+        self.assertEqual(linear_search([1, 2, 3, 4, 5], 1), 0)
+
 if __name__ == "__main__":
     unittest.main()
